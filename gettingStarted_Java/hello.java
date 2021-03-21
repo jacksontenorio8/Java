562e13e484ff360173025f0f32e8ed6be88495c0
Jackson <jacksontenorio8@gmail.com>
2021-03-21 19:09:49 -0300

    Update hello.java

--- gettingStarted_Java/hello.java
+++ gettingStarted_Java/hello.java
@@ -1,4 +1,7 @@
-/* Temos um método principal (main) que está dentro de uma Classe (public class hello) que faz parte de um pacote. Classes são compostas de métodos. E Pacotes são formados por um conjunto de Classes.*/
+/* 
+- Temos um método principal (main) que está dentro de uma Classe (public class hello) que faz parte de um pacote. - Classes são compostas de métodos. E Pacotes são formados por um conjunto de Classes.
+- print escreve na tela
+- println escreve na tela e pula uma linha*/
 
 public class hello {
     public static void main(String[] args) {
