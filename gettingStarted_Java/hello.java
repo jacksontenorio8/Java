f04d1c2d1623415a81c2b596aadc93892b91839d
Jackson <jacksontenorio8@gmail.com>
2021-03-21 18:52:51 -0300

    Update hello.java

--- gettingStarted_Java/hello.java
+++ gettingStarted_Java/hello.java
@@ -1,3 +1,5 @@
+// Temos um método principal (main) que está dentro de uma classe (public class hello) que faz parte de um pacote.
+
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
