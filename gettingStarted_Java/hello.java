e98e11beb4fb89a735e59eee7cd3c5a61a145314
Jackson <jacksontenorio8@gmail.com>
2021-03-21 20:46:32 -0300

    Update hello.java

--- gettingStarted_Java/hello.java
+++ gettingStarted_Java/hello.java
@@ -1,7 +1,12 @@
 /* 
 - Temos um método principal (main) que está dentro de uma Classe (public class hello) que faz parte de um pacote. - Classes são compostas de métodos. E Pacotes são formados por um conjunto de Classes.
 - print escreve na tela
-- println escreve na tela e pula uma linha*/
+- println escreve na tela e pula uma linha
+- No Camelcase sempre qua a 1ª letra estiver em maiúscula ela indica uma classe ou uma interface.
+- Se a 1ª letra for minúscula e as outras como camelcase ela pode ser: atributo, varável ou método.
+- O nome do pacote é escrito somente com letra minúscula.
+- O nome de uma constante é escrito com todas as letras em maiúsculo.
+*/
 
 public class hello {
     public static void main(String[] args) {
