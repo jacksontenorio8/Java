ff753e8ce62a4432450421b7d2ccabf0cfdb508b
Jackson <jacksontenorio8@gmail.com>
2021-03-22 15:57:22 -0300

    IdomaDoSistema.java
    Programa que diz o idioma do sistema.

---
+++ gettingStarted_Java/IdiomaDoSistema.java
@@ -0,0 +1,13 @@
+/*
+- Programa que diz o idioma do sistema.
+ */
+import java.util.Locale;
+
+public class IdiomaDoSistema {
+    public static void main(String[] args) {
+        Locale loc = Locale.getDefault();
+        System.out.println (loc.getDisplayLanguage());
+        System.out.println (loc.getLanguage());
+    }
+
+}
