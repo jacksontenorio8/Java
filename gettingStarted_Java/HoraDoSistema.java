ef74a787e51f1d628cc9b207e805dd876656d193
Jackson <jacksontenorio8@gmail.com>
2021-03-22 15:39:10 -0300

    HoraDoSistema.java
    Programa que diz a data e hora local.

---
+++ gettingStarted_Java/HoraDoSistema.java
@@ -0,0 +1,14 @@
+/*
+- Programa que mostra data e hora atual.
+- O uso da palavra "new" cria um novo objeto.
+*/
+
+import java.util.Date;
+
+public class HoraDoSistema {
+    public static void main(String[] args) {
+      Date data = new Date();  
+      System.out.println("A hora do sistema é");
+      System.out.println(data.toString());
+    }
+}
