4499dd9fb9fc9e34cbcf12d8d576e0b1cfdcc0b4
Jackson <jacksontenorio8@gmail.com>
2021-03-24 22:01:46 -0300

    Maven-vscode
    Aprendendo a usar maven no vscode.

---
+++ gettingStarted_Java/spring-vscode/src/test/java/com/jacksontenorio8/springvscode/SpringVscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacksontenorio8.springvscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
