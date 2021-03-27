4b9fd160869fd9295c4118cc323c416204ea2b6a
Jackson <jacksontenorio8@gmail.com>
2021-03-26 22:38:28 -0300

    Project003

---
+++ bootcamp_inter/start.java/project003/src/main/java/one/digitalinnovation/tipos/wrappers/wrappers.java
@@ -0,0 +1,38 @@
+package one.digitalinnovation.tipos.wrappes;
+
+public class wrappes {
+	
+	public static void main(String[] args) {
+	   //Autoboxing
+	   
+	   Byte b = 127;    //byte
+	   Byte b2 = -128;
+	   Byte nullByte = null;
+	   
+	   Character c = 'A';    //char
+	   Character c2 = 15;
+	   
+	   Short s = 32767;    //short
+	   Short s2 = -32768;
+	   
+	   Integer i = 2147483647;    //int
+	   Integer i2 = -2147483648;
+	   
+	   Long l = 9223372036854775807L;    //long
+	   Long l2 = -922337036854775808L;
+	   
+	   Float f = 65f; //float
+	   Float f2 = 65.0f;
+	   Float f3 = -0.5f
+	   
+	   Double d = 1024.99; //Double
+	   Double d2 = 10.2456;
+	   
+	   Bolean bo = true; //boolean
+	   Boolean bo2 = false;
+	   
+	   Boolean bo3 = Boolean.getBoolean(s:"false");    //OK
+	   Boolean bo4 = Boolean.valueOf("true");    //OK
+	
+	}
+}
