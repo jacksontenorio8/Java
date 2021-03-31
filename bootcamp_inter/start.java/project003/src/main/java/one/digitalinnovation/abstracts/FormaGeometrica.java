a6a746242d572cfd66cebe996f0173f023aac116
Jackson <jacksontenorio8@gmail.com>
2021-03-31 13:01:54 -0300

    FormaGeometrica.java
    Abstract  access modifier

---
+++ bootcamp_inter/start.java/project003/src/main/java/one/digitalinnovation/abstracts/FormaGeometrica.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.abstracts;
+
+public abstracts class FormaGeometrica {
+	
+	public abstracts String nome();
+	public abstracts Double area();
+	
+	public String desenha(int x, int y) { return "Desenhamos nas coordenadas X=$x Y:$y"; }
+}
