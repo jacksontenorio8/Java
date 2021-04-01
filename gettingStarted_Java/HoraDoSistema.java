fc06e96658e394fd82cf811f75dc1a92d1788f60
Jackson <jacksontenorio8@gmail.com>
2021-04-01 19:28:39 -0300

    modificação

--- gettingStarted_Java/HoraDoSistema.java
+++ gettingStarted_Java/HoraDoSistema.java
@@ -8,6 +8,7 @@ import java.util.Date;
 public class HoraDoSistema {
     public static void main(String[] args) {
       Date data = new Date();  
+      
       System.out.println("A hora do sistema é");
       System.out.println(data.toString());
     }
--- gettingStarted_Java/IdiomaDoSistema.java
+++ gettingStarted_Java/IdiomaDoSistema.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 public class IdiomaDoSistema {
     public static void main(String[] args) {
         Locale loc = Locale.getDefault();
+        
         System.out.println (loc.getDisplayLanguage());
         System.out.println (loc.getLanguage());
     }
