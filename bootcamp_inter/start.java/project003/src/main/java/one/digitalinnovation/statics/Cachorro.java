ab185e6580a6c32ffc04234571bc2ad2e2149236
Jackson <jacksontenorio8@gmail.com>
2021-04-02 11:36:45 -0300

    Project003
    Modificadores de acesso.

---
+++ bootcamp_inter/start.java/project003/src/main/java/one/digitalinnovation/statics/Cachorro.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.statics;
+
+public class Cachorro {
+	
+	public String zoologia = "Quadrúpede"; //Uma instância
+	//public static String zoologia = "Quadrúpede"; //Todas instância
+}
