8043d1dab4b1516901d0405a71253bc02cb9242e
Jackson <jacksontenorio8@gmail.com>
2021-04-02 12:23:35 -0300

    Project003
    Final access  modifier

---
+++ bootcamp_inter/start.java/project003/src/main/java/one/digitalinnovation/finals/Gamer.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.finals;
+
+public class Gamer {
+	
+	public String Keyboard() { return "Keyboard Gamer!"; }
+	
+	public final String mouse() { return "Mouse Gamer!"; }
+	
+}
