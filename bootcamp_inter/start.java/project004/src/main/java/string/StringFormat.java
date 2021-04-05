da51533cc94a206b216f6d9c18e122901740b66c
Jackson <jacksontenorio8@gmail.com>
2021-04-05 20:19:11 -0300

    Projec004
    Strings and the java.lang package.

--- bootcamp_inter/start.java/project004/src/main/java/string/StringFormat.java
+++ bootcamp_inter/start.java/project004/src/main/java/string/StringFormat.java
@@ -3,6 +3,17 @@ package string;
 public class StringFormat {
 
     public static void main(String[] args) {
-        
+
+        var nome = "Jackson";
+        var sobreNome = "Tenorio";
+        final var nomeCompleto = nome + " " + sobreNome;
+
+        System.out.println(nome);
+        System.out.println("Nome do Cliente : " + nome);
+        System.out.println("Nome completo do cliente : " + nomeCompleto);
+        final var mensagem = String.format("O cliente %s possui sobrenome %s ", nome, sobreNome);
+        System.out.println(mensagem);
+
+        System.out.println(String.format("Número %.2f", 1.2375d));
     }
 }
