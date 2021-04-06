53d9762aa9abecfe7841ffc8f2250b712442a2df
Jackson <jacksontenorio8@gmail.com>
2021-04-05 21:14:20 -0300

    Project004
    Final exercise.

---
+++ bootcamp_inter/start.java/project004/src/main/java/string/ExercicioFinal.java
@@ -0,0 +1,16 @@
+package string;
+
+public class ExercicioFinal {
+
+    public static void main(String[] args) {
+
+        var escola = "Digital Innovation One";
+
+        System.out.println(escola);
+        System.out.println("A B C D E F G".toCharArray());
+        System.out.println("Aula de Java ".split(" "));
+        System.out.println("Aula".concat("de Java"));
+        System.out.println("1234 asda qw".replaceAll("[0-9]", "#"));
+
+    }
+}
