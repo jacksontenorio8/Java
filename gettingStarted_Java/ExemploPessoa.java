76585f5c4b421cf8d108ba06212ff7b03a611e7f
Jackson <jacksontenorio8@gmail.com>
2021-04-07 18:04:04 -0300

    Java

---
+++ gettingStarted_Java/ExemploPessoa.java
@@ -0,0 +1,9 @@
+public class ExemploPessoa {
+
+    public static void main(String[] args) {
+       Pessoa pessoa = new Pessoa();
+       
+       System.out.println(pessoa.getNome());
+    }
+    
+}
---
+++ gettingStarted_Java/Pessoa.java
@@ -0,0 +1,8 @@
+public class Pessoa {
+
+    private String nome = "Jackson";
+
+    public String getNome() {
+        return nome;
+    }
+}
