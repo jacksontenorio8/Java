3ff3f9807840410c1501218aea4b962269654a00
Jackson <jacksontenorio8@gmail.com>
2021-04-20 15:51:13 -0300

    java

---
+++ gettingStarted_Java/JavaOnline/src/main/java/ImprimindoVariaveis.java
@@ -0,0 +1,10 @@
+public class ImprimindoVariaveis {
+    public static void main(String[] args){
+        int idadeDosPaisParaCadastro = 33;
+        System.out.println("A idade é = " +idadeDosPaisParaCadastro);
+    }
+}
+/***
+ * args é parametro de entrada do metódo main
+ *
+ * */
--- gettingStarted_Java/JavaOnline/src/main/java/OlaMundo.java
+++ gettingStarted_Java/JavaOnline/src/main/java/OlaMundo.java
@@ -1,6 +1,6 @@
 public class OlaMundo {
 
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         System.out.println("Olá diretamente do Intelij");
     }
 
