b01a861c7f8eca8bb20e1da1e7e77de05426f960
Jackson <jacksontenorio8@gmail.com>
2021-04-21 03:13:38 -0300

    Creat DadosPesoais.java

---
+++ gettingStarted_Java/JavaOnline/src/main/java/DadosPessoais.java
@@ -0,0 +1,13 @@
+public class DadosPessoais {
+
+    public static void main(String[] args){
+
+        String name = "Jackson";
+        String adress = "Av.Paraguassu, 37,";
+        String phoneNumber = "+551199999999";
+
+        System.out.println("O "+name+" domiciliado no endereço "+adress+" com o telefone "+phoneNumber+" não posui nenhum tipo de pendencia.");
+
+
+    }
+}
