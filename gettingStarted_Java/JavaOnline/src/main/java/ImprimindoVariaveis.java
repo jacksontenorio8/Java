21375a7d0a7649470ec2c4d4270c4b57d7663b18
Jackson <jacksontenorio8@gmail.com>
2021-04-21 02:32:36 -0300

    variaveis

--- gettingStarted_Java/JavaOnline/src/main/java/ImprimindoVariaveis.java
+++ gettingStarted_Java/JavaOnline/src/main/java/ImprimindoVariaveis.java
@@ -1,7 +1,19 @@
 public class ImprimindoVariaveis {
+
     public static void main(String[] args){
-        int idadeDosPaisParaCadastro = 33;
-        System.out.println("A idade é = " +idadeDosPaisParaCadastro);
+
+        int idade = 33;
+        double salarioDoubble = 3000;
+        float salarioFloat = 3000;
+        byte idadeByte = 127;
+        short idadeShort = 32767;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        long numeroGrande = 1000L;
+        char caractere = 26; //2 bytes
+        String nome = "Jackson";
+
+        System.out.println("A idade é = " +idade);
     }
 }
 /***
