91e898f1d89c1ae91e873946edec297df14e4afd
Jackson <jacksontenorio8@gmail.com>
2021-04-22 12:54:10 -0300

    Operadores

---
+++ gettingStarted_Java/JavaOnline/src/main/java/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+
+    public static void main(String[] args){
+
+        int numero1 = 70;
+        int numero2 = 30;
+        int soma = numero1 + numero2;
+
+        System.out.println("O valor da some é: "+soma);
+
+    }
+    
+}
