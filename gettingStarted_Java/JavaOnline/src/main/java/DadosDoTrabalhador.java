e7912adf3739ee942bdf6b259e9d0074caabcdc0
Jackson <jacksontenorio8@gmail.com>
2021-04-22 11:59:28 -0300

    java
    Programa que imprime informações sobre um trabalhador.

---
+++ gettingStarted_Java/JavaOnline/src/main/java/DadosDoTrabalhador.java
@@ -0,0 +1,18 @@
+/**
+ * Programa que imprime os dados de um trabalhador.
+ * */
+
+public class DadosDoTrabalhador {
+
+    public static void main(String[] args){
+
+        String name = "Jackson";
+        double salario = 3200.07;
+        String sexo = "M";
+        int idade = 33;
+        String estadoCivil = "solteiro";
+
+        System.out.println("O trabalhador "+name+" do sexo "+sexo+" idade "+idade+" estado civil "+estadoCivil+" e salario "+salario+" ,encontra-se empregado nesta empresa.");
+    }
+
+}
