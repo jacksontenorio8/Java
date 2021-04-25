8849786f904c55cc6038a6b588be965c9637bb90
Jackson <jacksontenorio8@gmail.com>
2021-04-25 14:02:25 -0300

    Operadores de Atribuição

---
+++ gettingStarted_Java/JavaOnline/src/main/java/OperadoresDeAtriuicao.java
@@ -0,0 +1,11 @@
+public class OperadoresDeAtriuicao {
+    public static void main(String[] args) {
+        // +=, -=, *=, /=, %=
+        int salario = 1800;
+        //salario = salario + 1000;
+        salario += 1000;
+
+        System.out.println(salario);
+
+    }
+}
