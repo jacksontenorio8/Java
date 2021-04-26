cd9afccefa3aef5c99f27e768a1023b7d534c18c
Jackson <jacksontenorio8@gmail.com>
2021-04-25 22:44:32 -0300

    Java Funcional

---
+++ gettingStarted_Java/java-funcional/src/main/java/br/com/digital/innovation/one/FatorialRecursivo/Aula.java
@@ -0,0 +1,9 @@
+package br.com.digital.innovation.one.FatorialRecursivo;
+
+public class Aula {
+    public static void main(String[] args) {
+        int valor = 10; //instrução
+        int resultado = valor * 3; //instrução
+        System.out.println("O resultado é: "+resultado); //instrução
+    }
+}
