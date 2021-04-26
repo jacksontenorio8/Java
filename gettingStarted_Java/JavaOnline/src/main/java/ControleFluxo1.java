e09e4615f476c3c9ec38306b0a8051259c2952bd
Jackson <jacksontenorio8@gmail.com>
2021-04-26 13:58:56 -0300

    Controle de Fluxo

---
+++ gettingStarted_Java/JavaOnline/src/main/java/ControleFluxo1.java
@@ -0,0 +1,10 @@
+public class ControleFluxo1 {
+    public static void main(String[] args) {
+        // Se tivermos uma extrussão de código não precisamos usar as chaves
+        int idade = 20;
+        float salario = 2000;
+        if(idade > 18){
+            System.out.println("Entrou no clube!");
+        }
+    }
+}
