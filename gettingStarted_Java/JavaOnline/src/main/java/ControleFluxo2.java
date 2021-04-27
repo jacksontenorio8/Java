647db4895a5ab4626ded98f870b598bad4eb2a22
Jackson <jacksontenorio8@gmail.com>
2021-04-27 01:20:23 -0300

    Controle de Fluxo 2
    Operador ternário

---
+++ gettingStarted_Java/JavaOnline/src/main/java/ControleFluxo2.java
@@ -0,0 +1,12 @@
+public class ControleFluxo2 {
+    public static void main(String[] args) {
+
+        int idade = 20;
+        String status;
+
+        status = idade < 18 ? "Não é adulto" : "É adulto" ;
+
+        System.out.println(status);
+    }
+
+}
