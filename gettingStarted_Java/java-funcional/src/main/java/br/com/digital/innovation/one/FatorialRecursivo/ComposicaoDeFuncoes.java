734e250a505b439f6038811e69383b91f7bf41a7
Jackson <jacksontenorio8@gmail.com>
2021-04-27 09:45:46 -0300

    java funcional

--- gettingStarted_Java/java-funcional/src/main/java/br/com/digital/innovation/one/FatorialRecursivo/ComposicaoDeFuncoes.java
+++ gettingStarted_Java/java-funcional/src/main/java/br/com/digital/innovation/one/FatorialRecursivo/ComposicaoDeFuncoes.java
@@ -4,12 +4,15 @@ import java.util.Arrays;
 
 public class ComposicaoDeFuncoes {
     public static void main(String[] args) {
-        int [] valores = {1,2,3,4};
-        Arrays.stream(valores);
-               // .filter(numero -> numero % 2 == 0)
-                //.map(numero -> numero * 2)
-                //.forEach(numero -> System.out.println(numero));
 
+        //Funcional
+        int [] valores = {1,2,3,4};
+        Arrays.stream(valores)
+                .filter(numero -> numero % 2 == 0)
+                .map(numero -> numero * 2)
+                .forEach(numero -> System.out.println(numero));
+
+        //Imperativo
         for(int i= 0; i < valores.length; i++){
             int valor = 0;
               if(valores[i] % 2 == 0) {
---
+++ gettingStarted_Java/java-funcional/src/main/java/br/com/digital/innovation/one/FatorialRecursivo/Imutabilidade.java
@@ -0,0 +1,4 @@
+package br.com.digital.innovation.one.FatorialRecursivo;
+
+public class Imutabilidade {
+}
