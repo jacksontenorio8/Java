c3223868736b1120246d7c7de8eb642cb31d0dd6
Jackson <jacksontenorio8@gmail.com>
2021-05-03 17:29:39 -0300

    Java

---
+++ gettingStarted_Java/JavaOnline/src/main/java/ControleFluxo5.java
@@ -0,0 +1,16 @@
+public class ControleFluxo5 {
+    public static void main(String[] args) {
+        /*Dado um valor de um carro descubra em quantas vezes ele pode ser parcelado.
+         * Porém as parcelas não podem ser menores que 1000.
+         * */
+
+        double valorTotal = 30000;
+        for (int parcela = 0; parcela <= valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela < 1000) {
+                break;
+            }
+            System.out.println("Parcela "+ parcela + " R$" +valorParcela);
+        }
+    }
+}
