23031658a3bb04f0332be07163a4229e0062ff1a
Jackson <jacksontenorio8@gmail.com>
2021-05-31 21:46:20 -0300

    Lista de idades

---
+++ gettingStarted_Java/ListasDeIdades.java
@@ -0,0 +1,17 @@
+public class ListasDeIdades {
+    public static void main(String[] args) {
+
+        int[] idades = new int[6]
+        idades[0] = 7;
+        idades[1] = 16;
+        idades[2] = 18;
+        idades[3] = 26;
+        idades[4] = 29;
+        idades[5] = 33;
+        System.out.println(idades.length);
+        for (int i = 0; i < 6; i++) {
+            System.out.println((i + 1) + " Eventos importantes aconteram quanto tinha esta idade: " + idades[]);
+        }
+    }
+
+}
