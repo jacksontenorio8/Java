88c1e5d9ca735936d32e94fc7d425e9f687dc978
Jackson <jacksontenorio8@gmail.com>
2021-06-01 13:37:51 -0300

    Update

---
+++ gettingStarted_Java/JavaOnline/src/main/java/pratica/CoresEspectroMagnetico.java
@@ -0,0 +1,18 @@
+package pratica;
+
+public class CoresEspectroMagnetico {
+    public static void main(String[] args) {
+        String[] cor = new String[6];
+        cor[0] = "Vermelho";
+        cor[1] = "Laranja";
+        cor[2] = "Amarelo";
+        cor[3] = "Verde";
+        cor[4] = "Azul";
+        cor[5] = "Violeta";
+        System.out.println(cor.length);
+        for (int i = 0; i< 6; i++){
+            System.out.println((i + 1) + " Cor do Espectro Magnético: " + cor[i]);
+        }
+    }
+
+}
--- gettingStarted_Java/JavaOnline/src/main/java/pratica/Pesos.java
+++ gettingStarted_Java/JavaOnline/src/main/java/pratica/Pesos.java
@@ -2,6 +2,20 @@ package pratica;
 
 public class Pesos {
     public static void main(String[] args) {
-        
+
+        double[] peso = new double[7];
+
+        peso[0] = 30.0;
+        peso[1] = 63.2;
+        peso[2] = 72.3;
+        peso[4] = 78.0;
+        peso[5] = 82.0;
+        peso[6] = 76.3;
+
+        System.out.println(peso.length);
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println((i + 1) + " Pesos: " + peso[i]);
+        }
     }
 }
