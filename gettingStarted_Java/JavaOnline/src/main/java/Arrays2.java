3ebcec20b1e243c871892bbc6d797955c030a7e6
Jackson <jacksontenorio8@gmail.com>
2021-05-31 21:00:08 -0300

    Update

--- gettingStarted_Java/JavaOnline/src/main/java/Arrays2.java
+++ gettingStarted_Java/JavaOnline/src/main/java/Arrays2.java
@@ -1,8 +1,18 @@
 public class Arrays2 {
     public static void main(String[] args) {
-        int[] idades = new int[3];
-        System.out.println("Idade 1: " + idades[0]);
-        System.out.println("Idade 2: " + idades[1]);
-        System.out.println("Idade 3: " + idades[2]);
+        // byte, short, int, long, float, double = 0
+        // char '\u0000'
+        // boolean false;
+        // reference null
+        String[] nomes = new String[4];
+        nomes[0] = "Goku";
+        nomes[1] = "Naruto";
+        nomes[2] = "Luffy";
+        nomes[3] = "Kurosaki";
+        System.out.println(nomes.length);
+        for (int i = 0; i < 4; i++) {
+            System.out.println((i + 1)+ " nomes: " + nomes[i]);
+        }
+
     }
 }
