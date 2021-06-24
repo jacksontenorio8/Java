6f9ef261226f9e4b4c22bbd35ef6d79b65710e5f
Jackson <jacksontenorio8@gmail.com>
2021-06-24 11:59:10 -0300

    Arrays multidimensionais

---
+++ gettingStarted_Java/JavaOnline/src/main/java/pratica/Arrays6.java
@@ -0,0 +1,20 @@
+package pratica;
+
+public class Arrays6 {
+    public static void main(String[] args) {
+        int[] arrint = {1, 2, 3};
+        int[] arrint2 = new int[3];
+        int[] arrint3 = new int[]{1, 2, 3};
+
+        int[][] dias = new int[3][];
+        dias[0] = new int[2];
+        dias[1] = new int[]{1, 2, 3};
+        dias[2] = new int[4];
+
+        for (int[] arr: dias){
+            for (int num : arr){
+                System.out.println(num);
+            }
+        }
+    }
+}
