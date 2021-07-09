2f19a5a05b022e0ff8b233b64a1a1b38dffef9ab
Jackson <jacksontenorio8@gmail.com>
2021-07-09 16:18:10 -0300

    Update

--- gettingStarted_Java/URI/1000.java
+++ gettingStarted_Java/URI/1000.java
@@ -1,4 +1,4 @@
-/*Bem-vindo ao URI Online Judge!
+/*
 
 O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
 
