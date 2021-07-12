dfb41984ad53f74f058e2f02fba742e12c9fe3f3
Jackson <jacksontenorio8@gmail.com>
2021-07-11 22:07:22 -0300

    Introdução a POO

--- gettingStarted_Java/JavaOnline/src/main/java/br/com/abc/javacore/introducaoclasses/Estudantes.java
+++ gettingStarted_Java/JavaOnline/src/main/java/br/com/abc/javacore/introducaoclasses/Estudantes.java
@@ -1,4 +1,7 @@
 package br.com.abc.javacore.introducaoclasses;
 
 public class Estudantes {
+    public String nome;
+    public String matricula;
+    public int idade;
 }
---
+++ gettingStarted_Java/JavaOnline/src/main/java/br/com/abc/javacore/introducaoclassestest/EstudanteTest.java
@@ -0,0 +1,4 @@
+package br.com.abc.javacore.introducaoclassestest;
+
+public class EstudanteTest {
+}
