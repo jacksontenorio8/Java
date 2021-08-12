a031772bc409895ecea62efb83bf55b3414e4f80
Jackson <jacksontenorio8@gmail.com>
2021-08-11 21:30:11 -0300

    Curso de POO Java

---
+++ CursoPOOJava/src/main/java/com/jacksontenorio/CursoPOOJava/CursoPooJavaApplication.java
@@ -0,0 +1,13 @@
+package com.jacksontenorio.CursoPOOJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoPooJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoPooJavaApplication.class, args);
+	}
+
+}
